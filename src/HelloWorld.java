b69ba6d0c652b1d6b62b3a55c6da010ccea98c36
Haeun <107106107@naver.com>
2024-03-12 11:03:51 +0900

    Day01,02 half

---
+++ src/HelloWorld.java
@@ -0,0 +1,41 @@
+/**
+ * 테스트 하기 위한 클래스입니다.
+ * @author 강하은
+ * @since 2024.03.11
+ * @version 0.1
+ */
+public class HelloWorld {
+	/**
+	 * API Document 생성용 주석
+	 * 이 메세드는 프로그램 시작 진입점입니다.
+	 * @param args 명령행 인수를 받습니다.
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!!!");
+		
+		// 1. 주석
+		/*
+		 * 여러 줄 주석
+		 */
+		
+		// 들여쓰기 : 하고싶은 부분 잡고 ctrl + i , ctrl shift f
+		
+		// 2. 블럭 {}
+		int a = 10;
+		int b = 20;
+		{
+			// 블록 만들어서 변수 사용 제한 할 수 있음
+			int c = 30;
+		}
+		
+		// 3. 식별자
+		int num = 10;
+		int Num = 20;
+		
+		
+
+
+	}
+
+}
