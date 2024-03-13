02b73686d80df7e495edcf2189e3194ec1db7c44
betterthani <107106107@naver.com>
2024-03-13 17:58:29 +0900

    피라미드

--- src/Test8.java
+++ src/Test8.java
@@ -7,13 +7,12 @@ public class Test8 {
 		int n = scan.nextInt(); // 회전 수
 		
 		for(int i = 0; i < n; i++) {
-			int a = scan.nextInt(); // 받아오는 값의 개수
-			int[] arr = new int[a];
+			int count = scan.nextInt(); // 받아오는 값의 개수
 			
 			int sum = 0; // 초기화
-			for(int j = 0; j < a; j++) {
-				arr[j] = scan.nextInt();
-				sum += arr[j];
+			for(int j = 0; j < count; j++) {
+				int num = scan.nextInt();
+				sum += num;
 			}
 			System.out.println(sum);
 		}
